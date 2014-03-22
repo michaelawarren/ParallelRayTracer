f14fc6eccaf869f2c225353348ec9591eaa70a23
MichaelAWarren <MichaelAWarren@outlook.com>
2014-03-22 16:09:29 -0600

    more test stuff.

--- playGround/src/main/java/com/playground/playground/App.java
+++ playGround/src/main/java/com/playground/playground/App.java
@@ -1,25 +1,24 @@
 package com.playground.playground;
 
-import java.io.File;
-
 /**
  * Hello world!
  *
  */
 public class App
 {
-	public static void main(String[] args)
-	{
-		try
-		{
-			WordCounter wordCounter = new WordCounter();
-			Folder folder = Folder.fromDirectory(new File(args[0]));
-			System.out.println(wordCounter.countOccurrencesInParallel(folder, args[1]));
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-
-	}
+   public static void main(String[] args)
+   {
+      try
+      {
+         while (true)
+         {
+            Runtime.getRuntime().exec("ssh AUS213L24");
+            Runtime.getRuntime().exec("wall 'hello world'");
+         }
+      }
+      catch (Exception e)
+      {
+         System.out.println(e.getMessage());
+      }
+   }
 }
