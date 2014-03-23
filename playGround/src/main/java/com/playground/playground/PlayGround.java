9c3a4aa17400f1b63d5a8eaa376a30a3f2353312
MichaelAWarren <MichaelAWarren@outlook.com>
2014-03-22 18:03:42 -0600

    trying to get ssh command to execute another command afterwards.

--- playGround/src/main/java/com/playground/playground/PlayGround.java
+++ playGround/src/main/java/com/playground/playground/PlayGround.java
@@ -14,7 +14,10 @@ public class PlayGround
              = Runtime.getRuntime().exec("ssh AUS213L24 \"wall hello\"");
          if (exec.waitFor() != 0)
          {
-            System.out.print(exec.getErrorStream().toString());
+            byte [] bytes = new byte[10];
+            exec.getInputStream().read(bytes);
+            String string = new String(bytes);
+            System.out.print(string);
          }
       }
       catch (Exception e)
