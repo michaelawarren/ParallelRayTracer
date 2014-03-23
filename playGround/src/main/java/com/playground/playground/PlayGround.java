1707168844509e83ca7d3ea92b06ccf9f3ccb581
MichaelAWarren <MichaelAWarren@outlook.com>
2014-03-22 20:39:29 -0600

    testing a new way to call SSH

--- playGround/src/main/java/com/playground/playground/PlayGround.java
+++ playGround/src/main/java/com/playground/playground/PlayGround.java
@@ -1,14 +1,39 @@
 //package com.playground.playground;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
 public class PlayGround
 {
    public static void main(String[] args)
    {
-      sshCall();
+      processSsh();
+   }
+
+   private static void processSsh()
+   {
+      long start = System.nanoTime();
+      try
+      {
+         ProcessBuilder processBuilder = new ProcessBuilder("ssh",  "-p215", "AUS213L24", "\"wall hello\"");
+         final Process process = processBuilder.start();
+         if (process.waitFor() != 0)
+         {
+            byte[] bytes = new byte[255];
+            process.getErrorStream().read(bytes, 0, 255);
+            String string = new String(bytes);
+            System.out.print(string);
+         }
+      }
+      catch (IOException ex)
+      {
+         System.out.println(ex.toString());
+      }
+      catch (InterruptedException ex)
+      {
+         System.out.println(ex.toString());
+      }
+      long end = System.nanoTime();
+      System.out.println(end - start);
    }
 
    private static void sshCall()
